10891f865304e6735e25f6a90b3ba6b9cd8d5e3f
MingPV <jeung.pavee@gmail.com>
2025-09-27 16:52:33 +0700

    update jwt token

--- internal/user/usecase/usecase.go
+++ internal/user/usecase/usecase.go
@@ -69,9 +69,9 @@ func (s *UserService) Login(email string, password string) (string, *entities.Us
 
 	// Generate JWT token
 	claims := jwt.MapClaims{
-		"user_id":   user.ID,
-		"user_info": user,
-		"exp":       time.Now().Add(time.Hour * 72).Unix(), // 3 days
+		"user_id": user.ID,
+		// "user_info": user,
+		"exp": time.Now().Add(time.Hour * 72).Unix(), // 3 days
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
@@ -169,9 +169,9 @@ func (s *UserService) LoginOrRegisterWithGoogle(userInfo map[string]interface{},
 
 	// Generate JWT token
 	claims := jwt.MapClaims{
-		"user_id":   user.ID,
-		"user_info": user,
-		"exp":       time.Now().Add(time.Hour * 72).Unix(), // 3 days
+		"user_id": user.ID,
+		// "user_info": user,
+		"exp": time.Now().Add(time.Hour * 72).Unix(), // 3 days
 	}
 
 	jwtToken := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
